08ef61ac99121303b3f352adf8480d973b71c8c2
DuChongyang <66300928+DuChongyang@users.noreply.github.com>
2021-01-28 23:18:54 +0800

    Add files via upload

---
+++ Supply_Chain/Supply_Chain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Supply_Chain
+//
+//  Created by 杜重阳 on 2021/1/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Supply_Chain/Supply_ChainTests/Supply_ChainTests.swift
@@ -0,0 +1,33 @@
+//
+//  Supply_ChainTests.swift
+//  Supply_ChainTests
+//
+//  Created by 杜重阳 on 2021/1/28.
+//
+
+import XCTest
+@testable import Supply_Chain
+
+class Supply_ChainTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
